3867c863a706e8eccb20d7019bfa56d6f776408d
Lhaniii <ilhan95210@icloud.com>
2024-02-19 13:50:37 +0000

    Hello,puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
-grcc -c fputs ("Programming is like building a multilingual puzzle)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
