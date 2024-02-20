2473a5db55dcbc09bdd2b60cdd1ff8474d54bbcf
Lhaniii <ilhan95210@icloud.com>
2024-02-20 20:53:06 +0000

    alphabet maj

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - alphabet
+ *
+ * Return: Always (succes)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+
+	for (alphabet = 'A' ; alphabet <= 'z' ; alphabet++)
+	{
+		putchar(alphabet);
+
+	}
+
+	putchar('\n');
+	return (0);
+}
