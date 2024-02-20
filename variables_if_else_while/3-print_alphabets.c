aa0625cbd5289efeb247b120cfbc9b20b7af0afb
Lhaniii <ilhan95210@icloud.com>
2024-02-20 21:07:13 +0000

    alphabet maj

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,7 @@ int main(void)
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		putchar(alphabet);
 
-	for (alphabet = 'A'; alphabet <= 'z'; alphabet++)
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 		putchar(alphabet);
 
 	putchar('\n');
