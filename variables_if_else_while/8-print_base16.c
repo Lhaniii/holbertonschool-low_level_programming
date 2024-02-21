c1a58ba8290a967d545e945d4bc605e6c55c60fc
Lhaniii <ilhan95210@icloud.com>
2024-02-21 10:02:48 +0000

    hexa

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - hexadecimal
+ *
+ * Return: Always (succes)
+ */
+int main(void)
+{
+	char number = 0;
+
+	for (number = '0'; number <= '15'; number++)
+		putchar(number);
+
+	putchar('\n');
+
+	return (0);
+}
