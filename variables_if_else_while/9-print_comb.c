fc434d8f81c67f84c999718737aa1482dae7258a
Lhaniii <ilhan95210@icloud.com>
2024-02-21 14:00:17 +0000

    number and ,

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - prints all possible cominations of single-digit numbers with ,
+ *
+ * Return: Always (succes)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+		if (number < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	return (0);
+}
