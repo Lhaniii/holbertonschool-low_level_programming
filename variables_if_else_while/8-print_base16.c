e739c422b5d2924e58c3c04cb48d1bd491e0b624
Lhaniii <ilhan95210@icloud.com>
2024-02-21 13:28:11 +0000

    hexa

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,12 +7,15 @@
 int main(void)
 {
 	int number;
-
+	{
 	for (number = '0'; number <= '9'; number++)
 		putchar(number);
-
-	for (number = "61"; number <= "66"; number++)
-		putchar(number);
+	}
+	char letter;
+	{
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+	}
 
 	putchar ('\n');
 	return (0);
