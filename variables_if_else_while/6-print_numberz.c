c820c96a94e44f7ac289fd644f12522e70784dcf
Lhaniii <ilhan95210@icloud.com>
2024-02-21 09:12:57 +0000

    numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - number - char
+ *
+ * Return: always (succes)
+ */
+int main(void)
+{
+	while (number <= 9)
+
+		for (number = '0'; number <= '9'; alphabet++)
+			putchar(number);
+
+	putchar('\n')
+
+	return (0);
+}
