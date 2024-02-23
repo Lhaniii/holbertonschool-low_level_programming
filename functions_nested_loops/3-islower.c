c72cdb6d129ce5030d846507b495adb542d58109
Lhaniii <ilhan95210@icloud.com>
2024-02-23 10:19:02 +0000

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - print x10 alphabet
+ * Return: Always (succes)
+ */
+int _islower(int c)
+{
+		if (c >= 'a'  && c <= 'z')
+			return (1);
+		else
+			return (0);
+}
