dbb3594b9d17f150409e43052bbcc1ea9a15b598
Lhaniii <ilhan95210@icloud.com>
2024-02-27 15:08:00 +0000

    triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_triangle - print square with #
+ * return: 0
+ * @size: size of square
+ */
+void print_triangle(int size)
+{
+	int d;
+	int s;
+
+	if (size > 0)
+	{
+		for (d = 0; d < size; d++)
+		{
+			for (s = size - d; s > 1 ; s--)
+			{
+				_putchar(' ');
+			}
+			for (s = 0; s <= d; s++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+	_putchar('\n');	
+			
+	}
+}
