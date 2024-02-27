b5a84015bbf833306d01769ee33a42b5206eda3c
Lhaniii <ilhan95210@icloud.com>
2024-02-27 14:05:00 +0000

    fiz buz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,15 +13,15 @@ int main(void)
 		if ((n % 3) == 0 && (n % 5) == 0)
 		{
 			printf("FizzBuzz");
-		}else if ((n % 3) == 0)
+		} else if ((n % 3) == 0)
 		{
 			printf("Fizz");
-		}else if ((n % 5) == 0)
+		} else if ((n % 5) == 0)
 		{
 			printf("Buzz");
-		}else
+		} else
 		{
-			printf("%d" ,n);
+			printf("%d",n);
 		}
 		if (n != 100)
 		{
