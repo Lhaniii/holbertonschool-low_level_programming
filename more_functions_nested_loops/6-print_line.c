50e45f6c96341033e5fe80ab9133fe4bd6291a27
Lhaniii <ilhan95210@icloud.com>
2024-02-27 09:01:52 +0000

    line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_line - print line
+ * Return: 0
+ * @n: int
+ */
+void print_line(int n)
+{
+	int y;
+
+	if (n <= 0)
+	{
+		_putchar ('\n');
+
+	}
+	else
+	{
+		for (y = 0; y <= n ; y++)
+		{
+		_putchar(95);
+		}
+
+		_putchar('\n');
+	}
+
+}
