91cf901db81849ff775960a822ff5e12539d79a0
Lhaniii <ilhan95210@icloud.com>
2024-02-27 14:01:39 +0000

    fiz buz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,8 +1,7 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * fizz buzz - fizz buzz
+ * main - fizz buzz
  * Return: 0:
  */
 int main(void)
@@ -14,18 +13,15 @@ int main(void)
 		if ((n % 3) == 0 && (n % 5) == 0)
 		{
 			printf("FizzBuzz");
-		}
-			else if ((n % 3) == 0)
+		}else if ((n % 3) == 0)
 		{
 			printf("Fizz");
-		}
-		else if ((n % 5) == 0)
+		}else if ((n % 5) == 0)
 		{
 			printf("Buzz");
-		}
-		else
+		}else
 		{
-			printf("%d" , n);
+			printf("%d" ,n);
 		}
 		if (n != 100)
 		{
