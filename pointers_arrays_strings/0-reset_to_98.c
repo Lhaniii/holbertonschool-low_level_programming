e8658382cca9b9c20b25b96f1d188c0106b9c563
Lhaniii <ilhan95210@icloud.com>
2024-02-28 09:56:05 +0000

    98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - retourné 98
+ * Return: n
+ * @n: pointer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN
+#define MAIN
+
+void reset_to_98(int *n);
+int _putchar(char c);
+#endif
