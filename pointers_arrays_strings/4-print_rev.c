b4d2c83dba4cb980a4fbc66de6d06f46fd53cfc4
Lhaniii <ilhan95210@icloud.com>
2024-02-29 13:26:10 +0000

    blablabla

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,17 @@
 #include "main.h"
-#include "main.h"
+
 /**
- * print_rev - prints a string, followed by a new line
- * Return: 0
+ * print_rev - print in revers
+ *
+ * Return: void
+ *
  * @s: pointer
  */
 void print_rev(char *s)
 {
 	if (*s > '\0')
 	{
-	print_rev(s + 1);
-	_putchar(*s);
+		_putchar(s + 1);
+		_putchar(*s);
 	}
 }
