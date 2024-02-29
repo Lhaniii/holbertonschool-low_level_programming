cf2579c3954e256ffdf9468e6b11aa497e2a738f
Lhaniii <ilhan95210@icloud.com>
2024-02-29 16:48:35 +0000

    reverse

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,13 +8,13 @@ void rev_string(char *s)
 {
 	int a, b, c;
 
-	char temp; 
+	char temp;
 
 	for (a = 0; s[a] != '\0'; a++)
 	{
 	}
-	c = a -1;
-	
+	c = a - 1;
+
 	for (b = 0; b < a / 2; b++)
 	{
 	temp = s[b];
