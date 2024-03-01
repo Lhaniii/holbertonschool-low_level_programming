611fcdf32ac39445f4481a64ee7fabb7271eb774
Lhaniii <ilhan95210@icloud.com>
2024-03-01 21:21:59 +0000

    task 9 completed

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcpy - Copy a string
+ * 
+ * Return: 0 
+ * 
+ * @dest: Destination value
+ *
+ * @src: source value
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+
+	dest[a++] = '\0';
+
+	return (dest);
+}
