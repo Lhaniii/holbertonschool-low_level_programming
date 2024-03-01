9084c50ecf465b67b3390fdd34ea9509d07e20eb
Lhaniii <ilhan95210@icloud.com>
2024-03-01 10:23:04 +0000

    56789

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -11,13 +11,21 @@ void puts_half(char *str)
 	int a, b;
 	
 	for (b = 0; str[b] != '\0'; b++)
+	
+	if (b % 2 == 1)
 	{
+		a = (b - 1) / 2;
+		a += 1;
+	}
+	else
+	{
+		a = b / 2;
 	}
 
-	for (a = 5; a < b; a++)
+	for (; a < b; a++)
 	{
 		_putchar(str[a]);
 	}
-
+	
 	_putchar('\n');
 }
