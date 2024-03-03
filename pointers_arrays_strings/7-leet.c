94f581d0e3547f39b68f633a9a027851aa79021c
Lhaniii <ilhan95210@icloud.com>
2024-03-03 21:27:26 +0000

    3h

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * leet - Write a function that encodes a string into 1337.
+ *
+ * Return: s
+ *
+ * @s: string
+ */
+char *leet(char *s)
+{
+	int a = 0, b = 0, c = 5;
+	char st[5] = {'A' , 'E', '0', 'T', 'L'};
+	char sr[5] = {'4', '3', '0', '7', '1'};
+
+	while (s[a])
+	{
+		b = 0;
+
+		while (b < c)
+		{
+			if (s[a] == st[b] || s[a] - 32 == st[b])
+			{
+				s[a] = sr[b];
+			}
+
+			b++;
+		}
+		a++;
+	}
+	return (s);
+}
