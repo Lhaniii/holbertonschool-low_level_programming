97438248999c6091525c6c91f74c609e827cfcd2
Lhaniii <ilhan95210@icloud.com>
2024-03-04 14:07:04 +0000

    task 3 complete

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strspn -Write a function that gets the length of a prefix substring.
+ *
+ * Return: c
+ *
+ * @s: char 
+ *
+ * @accept: char
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int a, b, c = 0;
+
+	for (a = 0; s[a]; a++)
+	{
+		for (b = 0; accept[b]; b++)
+		{
+			if (s[a] == accept[b])
+			{
+				c++;
+				break;
+			}
+		}
+		if (s[a] != accept[b])
+		{
+			return (c);
+		}
+	}
+	return (c);
+}
