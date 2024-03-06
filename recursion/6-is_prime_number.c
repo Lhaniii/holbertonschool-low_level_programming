2c2b80a050af7694a0ca27b94eaa7c7b2f22318a
Lhaniii <ilhan95210@icloud.com>
2024-03-06 16:07:45 +0000

    task 6 completed

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -8,7 +8,7 @@
  */
 int is_prime_number(int n)
 {
-        return (prime(n, 1));
+	return (prime(n, 1));
 }
 
 /**
@@ -22,11 +22,11 @@ int is_prime_number(int n)
  */
 int prime(int n, int a)
 {
-        if (n <= 1)
-                return (0);
-        if (n % a == 0 && a > 1)
-                return (0);
-        if ((n / a) < a)
-                return(1);
-        return (prime(n, a + 1));
+	if (n <= 1)
+		return (0);
+	if (n % a == 0 && a > 1)
+		return (0);
+	if ((n / a) < a)
+		return (1);
+	return (prime(n, a + 1));
 }
