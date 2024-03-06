3fbc699d907e485acd48681f35047302542295b0
Lhaniii <ilhan95210@icloud.com>
2024-03-06 10:47:46 +0000

    task 3 completed

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -12,10 +12,11 @@ int factorial(int n)
 	{
 		return (-1);
 	}
-	if ( n <= 1)
+	if (n <= 1)
 	{
-		return(1);
+		return (1);
 	}
 
-	return (n * factorial( n - 1));
+	return (n * factorial(n - 1));
+
 }
