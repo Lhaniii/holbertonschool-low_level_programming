bf6e46335e0f4dea7bb02adec3f1a775b8bea10d
Lhaniii <ilhan95210@icloud.com>
2024-03-11 11:05:30 +0000

    array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Write a function that creates an array of chars.
+ *
+ * @size: int
+ *
+ * @c: char
+ *
+ * Return:
+ */
+char *create_array(unsigned int size, char c);
+{
