ea8e39a16511a10d4bf171c26d910aba122e6fbc
Lhaniii <ilhan95210@icloud.com>
2024-03-19 15:15:15 +0000

    file

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Write a function that returns the sum of all its parameters.
+ *
+ * @n : int
+ *
+ * Retun:
+ */
+int sum_them_all(const unsigned int n, ...)
+{
