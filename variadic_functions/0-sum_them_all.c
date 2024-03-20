045da954e28f29b8dae559d428cc851d109bf101
Lhaniii <ilhan95210@icloud.com>
2024-03-20 16:19:38 +0000

    task 0 complete

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -5,7 +5,26 @@
  *
  * @n : int
  *
- * Retun:
+ * Return: 0
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list args;
+	unsigned int i;
+	int sum = 0;
+
+	if (n != 0)
+	{
+		va_start(args, n);
+
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(args, int);
+		}
+
+		va_end(args);
+		return (sum);
+	}
+
+	return (0);
+}
