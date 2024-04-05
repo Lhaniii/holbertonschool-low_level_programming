a90cd746acb7689f8e028358fc133e8cd1dbb7df
Lhaniii <ilhan95210@icloud.com>
2024-04-05 17:46:55 +0000

    Write a function that prints the binary representation of a number.

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN
 #define MAIN
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,4 +9,5 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int _putchar(char c);
 #endif
